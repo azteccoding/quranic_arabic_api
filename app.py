07930fa74fdd55428aa2c852c530943faff9a9f7
aztecdev <contactos@mnaztecdev.com>
2025-04-17 23:35:28 -0600

    initial upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from pymongo import MongoClient
+from bson.json_util import dumps
+import pyarabic.araby as araby
+from dotenv import dotenv_values
+import os
+
+
+# Env variables (passwords)
+config = dotenv_values(".env")
+
+#Flask and MongoDB setup
+app = Flask(__name__)
+client = MongoClient(config["MONGO_URI"])
+db = client.quranic_arabic
+dictionary = db.dictionary
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return dumps("search_result")
+
+
+@app.route("/arabic/<kalam>", methods=["GET"])
+def find_arabic_word(kalam):
+    after_filter = araby.strip_diacritics(kalam)
+    search_result = dictionary.find({"arabic_sg": after_filter})
+    return dumps(search_result)
+
+
+@app.route("/spanish/<word>", methods=["GET"])
+def find_spanish_word(word):
+    search_result = dictionary.find({"spanish": word})
+    return dumps(search_result)
+
+
+if __name__ == "__main__":
+    app.run()
